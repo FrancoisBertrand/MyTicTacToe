d28b5f1eea7661925771d1c3a87cc09cd0bd4deb
Francois <s0553643@htw-berlin.de>
2016-12-12 18:27:23 +0100

    no message

--- app/src/main/java/de/mobileanwendungen/mytictactoe/MainActivity.java
+++ app/src/main/java/de/mobileanwendungen/mytictactoe/MainActivity.java
@@ -1,13 +1,22 @@
 package de.mobileanwendungen.mytictactoe;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counter;
+    private int xPosition = 0;
+    private int yPosition = 0;
+    private Gameboard gameboard = null;
+    private String spielerX = "X";
+    private String spielerO = "O";
+    private boolean spielende = false;
 
 
 
@@ -16,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().hide();
+        gameboard = new Gameboard();
+        counter = 0;
     }
+
+
 
 
 
