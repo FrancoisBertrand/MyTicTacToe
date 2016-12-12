8d844994b7599a46ea1927910a7e42f37fedd263
Francois <s0553643@htw-berlin.de>
2016-12-12 15:40:40 +0100

    view fertiggestellt

---
+++ app/src/main/java/de/mobileanwendungen/mytictactoe/MainActivity.java
@@ -0,0 +1,24 @@
+package de.mobileanwendungen.mytictactoe;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Verhindert das Drehen der View
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
